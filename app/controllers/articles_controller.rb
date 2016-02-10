174c75827c2939acd87895521385f8824588255d
voDox17 <divesh7chadha@gmail.com>
2016-02-10 10:50:59 +0000

    added 'edit' functionality with validation checks

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+
 
   private
   def article_params
