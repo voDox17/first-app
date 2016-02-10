d3beb4f42d3486bcc89748330b9a6856865da5da
voDox17 <divesh7chadha@gmail.com>
2016-02-10 09:58:44 +0000

    added the create functionality to the alpha-blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,21 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+   if  @article.save
+     flash[:notice] = "article successfully created"
+    redirect_to article_path(@article)
+   else
+     render 'new'
+   end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
   def article_params
-    params.require(:article).permit(:topic, :description)
+    params.require(:article).permit(:topic, :description, :city, :country)
   end
 
 end
