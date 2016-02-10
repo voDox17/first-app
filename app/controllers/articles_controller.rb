96b77aa39fd63b357d759b1038f507215116f384
voDox17 <divesh7chadha@gmail.com>
2016-02-10 17:41:40 +0000

    created links and listed the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
 
@@ -19,6 +22,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   def edit
+    
     @article = Article.find(params[:id])
   end
   def update
