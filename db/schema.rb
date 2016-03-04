e70fb2fb9e3c05b3ef9a44740b8ae91cc6765d83
voDox17 <divesh7chadha@gmail.com>
2016-03-04 07:32:29 +0000

    add secure password via bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304043908) do
+ActiveRecord::Schema.define(version: 20160304070845) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "topic"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160304043908) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
