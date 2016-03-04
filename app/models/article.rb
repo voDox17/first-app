6c8238b283598ec92d6f1b91cc49ca5b5c1ac2eb
voDox17 <divesh7chadha@gmail.com>
2016-03-04 05:57:42 +0000

    add users_id to article and debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :topic, presence: true, length: {minimum: 4, maximum: 15}
   validates :description, presence: true, length: {minimum: 10, maximum: 50}
   validates :city, presence: true, length: {minimum: 2, maximum: 18}
   validates :country, presence: true, length: {minimum: 3, maximum: 20}
+  validates :user_id, presence: true
 end
